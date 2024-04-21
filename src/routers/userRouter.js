f156dea72912c5c809fea0d09986f78d57f035d6
Ivan Ulloa <dgmnicolas@gmail.com>
2024-04-21 13:57:13 -0400

    PF

---
+++ src/routers/userRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/userController');
+const upload = require('../utils/multer');
+const { isAdmin } = require('../middleware/authorization');
+
+module.exports = function (io) {
+    router.post('/premium/:uid', isAdmin, UserController.changeUserRole);
+    router.post('/:uid/documents', upload.fields([
+        { name: 'documents',},
+        { name: 'profile',},
+        { name: 'products',}, 
+        { name: 'comprobante de domicilio',}, 
+        { name: 'comprobante de estado de cuenta',}, 
+        { name: 'identificacion',},  
+    ]), UserController.uploadDocuments);
+    return router;
+}
